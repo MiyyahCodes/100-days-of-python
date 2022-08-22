3f8db90515234e5dc64d196d3e80c9c7d9777886
Adebimpe Islamiyyah Adefila <95176308+MiyyahCodes@users.noreply.github.com>
2022-08-22 13:59:01 +0100

    Update MiyyahBrandNameGenerator.py

--- day-1/MiyyahBrandNameGenerator.py
+++ day-1/MiyyahBrandNameGenerator.py
@@ -1,1 +1,9 @@
-#write your code here
+#1. Create a greeting for your program.
+print("Welcome to Miyyah Band Name Generator!")
+#2. Ask the user for the city that they grew up in.
+cityName = input("What's the name of the city you grew up in?\n")
+#3. Ask the user for the name of a pet.
+petName = input("What is your pet's name?\n")
+#4. Combine the name of their city and pet and show them their band name.
+bandName = "Your band name could be " + cityName + " " + petName
+print(bandName)
