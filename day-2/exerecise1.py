d5db0a4475d2e0f9469b113a8a55763d44ead923
Adebimpe Islamiyyah Adefila <95176308+MiyyahCodes@users.noreply.github.com>
2022-08-24 11:29:45 +0100

    Create exerecise1.py

---
+++ day-2/exerecise1.py
@@ -0,0 +1,11 @@
+# 🚨 Don't change the code below 👇
+two_digit_number = input("Type a two digit number: ")
+# 🚨 Don't change the code above 👆
+
+####################################
+
+first_num = int(two_digit_number[0])
+second_num = int(two_digit_number[1])
+
+add = first_num + second_num
+print(add)
