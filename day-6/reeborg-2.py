d3cb12b70dcafac6e558bfc69357d8e6e6baeeda
Adebimpe Islamiyyah Adefila <95176308+MiyyahCodes@users.noreply.github.com>
2022-09-20 13:53:53 +0100

    Create reeborg-2.py

---
+++ day-6/reeborg-2.py
@@ -0,0 +1,30 @@
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+    
+def turn_around():
+    turn_right()
+    move()
+    turn_right()
+    move()
+    turn_left()
+    move()
+    turn_left()
+    move()
+    
+def last_step():
+    turn_right()
+    move()
+    turn_right()
+    move()
+    
+move()
+turn_left()
+move()
+turn_around()
+turn_around()
+turn_around()
+turn_around()
+turn_around()
+last_step()
